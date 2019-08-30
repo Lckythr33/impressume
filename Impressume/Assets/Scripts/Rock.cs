400809d835479b3aa101b112e8569b3f4c30d33a
devBeast <Zeeshawn1@gmail.com>
2019-08-30 12:50:48 -0700

    Working with more paths

---
+++ Impressume/Assets/Scripts/Rock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+
+    [SerializeField] int health = 500;
+  
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+ 
+}
---
+++ Impressume/Assets/Scripts/Spin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    [SerializeField] float speedOfSpin = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, speedOfSpin * Time.deltaTime);
+    }
+}
